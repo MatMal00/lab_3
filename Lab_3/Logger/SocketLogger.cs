ec14bf2952e5b521ea0586128534177a0a3c9101
MatMal00 <101005328+MatMal00@users.noreply.github.com>
2022-04-02 15:35:49 +0200

    Dodaj pliki projektów.

---
+++ Lab_3/Logger/SocketLogger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3.Logger
+{
+    class SocketLogger: ILogger
+    {
+        private ClientSocket clientSocket;
+        private bool disposedValue;
+
+        public SocketLogger(string host, int port)
+        {
+
+        }
+
+        public void Log(params string[] messages)
+        {
+
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    clientSocket.Dispose();
+                }
+
+                // TODO: Zwolnić niezarządzane zasoby (niezarządzane obiekty) i przesłonić finalizator
+                // TODO: Ustawić wartość null dla dużych pól
+
+                disposedValue = true;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            // Nie zmieniaj tego kodu. Umieść kod czyszczący w metodzie „Dispose(bool disposing)”.
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~SocketLogger()
+        {
+            Dispose(disposing: false);
+        }
+    }
+}

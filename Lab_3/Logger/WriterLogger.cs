941520e81f6abfee0ba3114ad715fa05f76dfb0b
Mateusz Malec <mateusz.malec@microsoft.wsei.edu.pl>
2022-04-22 21:26:06 +0200

    feat: move date variable to log method

--- Lab_3/Logger/WriterLogger.cs
+++ Lab_3/Logger/WriterLogger.cs
@@ -6,11 +6,10 @@ namespace Lab_3.Logger
     public abstract class WriterLogger : ILogger
     {
         protected TextWriter writer;
-        protected DateTime time = DateTime.Now;
 
         public virtual void Log(params string[] messages)
         {
-            string messageToSend = time.ToString("yyyy-MM-ddTHH:mm:sszzz") + ": ";
+            string messageToSend = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:sszzz") + ": ";
             foreach (var message in messages) messageToSend += message + " ";
 
             writer.WriteLine(messageToSend);
